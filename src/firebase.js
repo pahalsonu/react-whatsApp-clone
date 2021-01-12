a2cfc1ce45a905168b6f3f1f249070bd8c124c01
pahalsonu <pahalsonu@gmail.com>
2021-01-12 16:17:17 +0530

    awesome and fast whatsapp clone

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD9pzDtf1JLO8__RMDdaEl6KDiQ2od-ssk",
+    authDomain: "neat-element-298114.firebaseapp.com",
+    projectId: "neat-element-298114",
+    storageBucket: "neat-element-298114.appspot.com",
+    messagingSenderId: "941901177811",
+    appId: "1:941901177811:web:3bc9203f0487ac12958526"
+  };
