a32331f36d292b501929128e7515c65f476e2263
pahalsonu <pahalsonu@gmail.com>
2021-01-13 21:41:06 +0530

    chat header and body ui done

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import React from 'react';
 //import component
 import Sidebar from './Sidebar'
 import './App.css';
-
+import Chat from './Chat'
 function App() {
   return (
     // BEM naming convention
     <div className="app">
       <div className="app_body">
         <Sidebar />
-        {/* Sidebar */}
-        {/* Chat */}
+        <Chat />
+
       </div>
     </div>
   );
